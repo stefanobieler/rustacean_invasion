0a9677b78f45c536ccd8d50ed51bf90c8d4c9c8a
Stefano Bieler <stefanobieler@gmail.com>
2022-04-06 18:22:36 -0700

    window created, and assets inserted

--- rustacean_invasion/src/main.rs
+++ rustacean_invasion/src/main.rs
@@ -1,3 +1,17 @@
+use bevy::prelude::*;
+
+/// Main player sprite.
+const PLAYER_SPRITE: &str = "player_a_01.png";
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor{
+            title: "Rustacean Invasion".to_string(),
+            width: 598.0,
+            height: 676.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
